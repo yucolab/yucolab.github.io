555081464583dcce6efcf0a03f85091056d9013c
JIMz <jimz@YCL-0000-1187.local>
2022-11-25 11:43:28 +0800

    remove index.html in root

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # yucolab.github.io
 
-github page test here.
+## workflow
+- run `gatsby develop` inside `gatsby/` ; 
+- update related files in `/gatsby/src` ; 
+- run `/build.zsh` ; 
+- git add / comment / push ; 
+
+## folder structure
+- CNAME
+  - pages.yucolab.com
+
+- root
+  - docs/
+  - configured inside github
+
+- gendering engine
+  - gatsby
 
--- build.zsh
+++ build.zsh
@@ -1,9 +1,31 @@
 #!/usr/local/bin/zsh
 
+#/////////////////////////////////////////////
+function banner
+{
+  message="$1"
+  line="------------------------------"
+    
+    echo "$line"
+    echo "$message"
+    echo "$line"
+}
+#/////////////////////////////////////////////
+
+banner "change into the dir gatsby"
 cd gatsby
 
+banner "start a new build"
 npm run build
+
+banner "sync the public folder to docs"
 npm run sync
 
+banner "move back to the script dir"
+cd -
 
+banner "copy CNAME to docs/"
+cp CNAME ./docs/
+
+banner "done ~"
 
